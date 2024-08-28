9e435158143e3f95bbd30d230cb243ad71d677fd
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-08-28 10:57:47 -0300

    chore: permissions by role

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,7 +1,9 @@
 import { AbilityBuilder, CreateAbility, createMongoAbility, ForcedSubject, MongoAbility } from '@casl/ability';
+import { User } from './models/user';
+import { permissions } from './permissions';
 
-const actions = ['manage', 'invite', 'delete'] as const
-const subjects = ['User', 'all'] as const
+const actions = ['manage', 'invite', 'create', 'delete'] as const
+const subjects = ['User', 'Project', 'all'] as const
 
 type AppAbilities = [
     (typeof actions)[number],
@@ -14,8 +16,16 @@ type AppAbilities = [
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAbility = createMongoAbility as CreateAbility<AppAbility>
 
-const { build, can } = new AbilityBuilder(createAbility)
+export function defineAbilityFor(user: User) {
+    const builder = new AbilityBuilder(createAbility)
 
-can('invite', 'User')
+    if (typeof permissions[user.role] !== 'function') {
+        throw new Error(`Permissions for role ${user.role} not found.`)
+    }
 
-export const ability = build()
+    permissions[user.role](user, builder)
+
+    const ability = builder.build()
+
+    return ability
+}
