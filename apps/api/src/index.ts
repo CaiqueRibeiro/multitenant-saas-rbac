e431e2e774a60c7e4675b6a3da6b47565f7f9a09
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-08-28 10:32:43 -0300

    chore: initial config of CASL

---
+++ apps/api/src/index.ts
@@ -0,0 +1,7 @@
+import { ability } from '@saas/auth';
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteUser = ability.can('delete', 'User')
+
+console.log(userCanInviteSomeoneElse)
+console.log(userCanDeleteUser)
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,1 +1,21 @@
-const a: string = "teste"
+import { AbilityBuilder, CreateAbility, createMongoAbility, ForcedSubject, MongoAbility } from '@casl/ability';
+
+const actions = ['manage', 'invite', 'delete'] as const
+const subjects = ['User', 'all'] as const
+
+type AppAbilities = [
+    (typeof actions)[number],
+    (
+        | (typeof subjects)[number]
+        | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
+    ),
+]
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAbility = createMongoAbility as CreateAbility<AppAbility>
+
+const { build, can } = new AbilityBuilder(createAbility)
+
+can('invite', 'User')
+
+export const ability = build()
