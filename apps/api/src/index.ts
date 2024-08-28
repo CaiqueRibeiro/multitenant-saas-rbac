b44621ed1774716a96cfd39f5ad7a209f94fe704
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-08-28 20:23:02 -0300

    feature: create user endpoint boilerplate

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor } from '@saas/auth';
-
-const ability = defineAbilityFor({ role: 'ADMIN' })
-
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-const userCanDeleteUser = ability.can('delete', 'User')
-
-console.log(userCanInviteSomeoneElse)
-console.log(userCanDeleteUser)
