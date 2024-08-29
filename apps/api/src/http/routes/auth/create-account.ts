50196f3ba47284da7a6290b0c7c3ed8bf823ee9d
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-08-29 09:25:06 -0300

    feature: create user

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from "fastify"
 import type { ZodTypeProvider } from "fastify-type-provider-zod"
+import { hash } from 'bcryptjs'
 import { z } from "zod"
+import { prisma } from "@/lib/prisma"
 
 const bodySchema = z.object({
     name: z.string(),
@@ -11,7 +13,23 @@ const bodySchema = z.object({
 export async function createAccount(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post('/users', {
         schema: { body: bodySchema }
-    }, () => {
-        return 'User created!'
+    }, async (request, reply) => {
+        const { name, email, password } = request.body
+
+        const userWithSameEmail = await prisma.user.findUnique({
+            where: { email }
+        })
+
+        if (userWithSameEmail) {
+            return reply.status(400).send({ message: 'user with same e-mail already exists' })
+        }
+
+        const passwordHash = await hash(password, 6)
+
+        await prisma.user.create({
+            data: { name, email, passwordHash }
+        })
+
+        return reply.status(201).send()
     })
 }
