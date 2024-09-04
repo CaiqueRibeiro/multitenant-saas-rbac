e1c054470afd057cb97241a293bd5b2007b16354
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-09-04 17:43:09 -0300

    nextjs frontend boilerplate

--- apps/api/src/http/routes/auth/reset-password.ts
+++ apps/api/src/http/routes/auth/reset-password.ts
@@ -1,55 +1,61 @@
-import type { FastifyInstance } from "fastify"
-import type { ZodTypeProvider } from "fastify-type-provider-zod"
 import { compare, hash } from 'bcryptjs'
-import { z } from "zod"
-import { prisma } from "@/lib/prisma"
-import { BadRequestError } from "../_errors/bad-request-error"
-import { UnauthorizedError } from "../_errors/unauthorized-error"
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
 
 const bodySchema = z.object({
     code: z.string(),
-    password: z.string().min(6)
+    password: z.string().min(6),
 })
 
 export async function resetPassword(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().post('/password/reset', {
-        schema: {
-            tags: ['Auth'],
-            summary: 'Reset user password',
-            body: bodySchema,
-            response: {
-                204: z.null()
-            }
-        }
-    }, async (request, reply) => {
-        const { code, password } = request.body
+    app.withTypeProvider<ZodTypeProvider>().post(
+        '/password/reset',
+        {
+            schema: {
+                tags: ['Auth'],
+                summary: 'Reset user password',
+                body: bodySchema,
+                response: {
+                    204: z.null(),
+                },
+            },
+        },
+        async (request, reply) => {
+            const { code, password } = request.body
 
-        const tokenFromCode = await prisma.token.findUnique({
-            where: { id: code }
-        })
-
-        if (!tokenFromCode) {
-            throw new UnauthorizedError()
-        }
-
-        if (tokenFromCode.type !== 'PASSWORD_RECOVER') {
-            throw new BadRequestError('Invalid token.')
-        }
-
-        const passwordHash = await hash(password, 6)
-
-        await prisma.$transaction([
-            prisma.user.update({
-                where: { id: tokenFromCode.userId },
-                data: {
-                    passwordHash
-                }
-            }),
-            prisma.token.delete({
-                where: { id: code }
+            const tokenFromCode = await prisma.token.findUnique({
+                where: { id: code },
             })
-        ])
 
-        return reply.status(204).send()
-    })
+            if (!tokenFromCode) {
+                throw new UnauthorizedError()
+            }
+
+            if (tokenFromCode.type !== 'PASSWORD_RECOVER') {
+                throw new BadRequestError('Invalid token.')
+            }
+
+            const passwordHash = await hash(password, 6)
+
+            await prisma.$transaction([
+                prisma.user.update({
+                    where: { id: tokenFromCode.userId },
+                    data: {
+                        passwordHash,
+                    },
+                }),
+                prisma.token.delete({
+                    where: { id: code },
+                }),
+            ])
+
+            return reply.status(204).send()
+        },
+    )
 }
--- apps/api/src/http/routes/invites/get-pending-invites.ts
+++ apps/api/src/http/routes/invites/get-pending-invites.ts
@@ -1,69 +1,79 @@
-import { auth } from "@/http/middlewares/auth";
-import { FastifyInstance } from "fastify";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
-import { z } from "zod";
-import { prisma } from "@/lib/prisma";
-import { getUserPermissions } from "@/utils/get-user-permissions";
-import { UnauthorizedError } from "../_errors/unauthorized-error";
-import { roleSchema } from "@saas/auth";
-import { NotFoundError } from "../_errors/not-found-error";
+import { roleSchema } from '@saas/auth'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+
+import { NotFoundError } from '../_errors/not-found-error'
 
 const getInvitesSchema = z.object({
-    invites: z.array(z.object({
-        id: z.string(),
-        email: z.string().email(),
-        role: roleSchema,
-        createdAt: z.date(),
-        author: z.object({
-            id: z.string().uuid(),
-            name: z.string().nullable(),
-        }).nullable()
-    }))
+  invites: z.array(
+    z.object({
+      id: z.string(),
+      email: z.string().email(),
+      role: roleSchema,
+      createdAt: z.date(),
+      author: z
+        .object({
+          id: z.string().uuid(),
+          name: z.string().nullable(),
+        })
+        .nullable(),
+    }),
+  ),
 })
 
 export async function getPendingInvites(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().register(auth).get('/pending-invites', {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/pending-invites',
+      {
         schema: {
-            tags: ['Invites'],
-            summary: 'Get all user pending invites',
-            security: [{ bearerAuth: [] }],
-            response: {
-                201: getInvitesSchema
-            }
-        }
-    }, async (request, reply) => {
+          tags: ['Invites'],
+          summary: 'Get all user pending invites',
+          security: [{ bearerAuth: [] }],
+          response: {
+            201: getInvitesSchema,
+          },
+        },
+      },
+      async (request, reply) => {
         const userId = await request.getCurrentUserId()
 
         const user = await prisma.user.findUnique({
-            where: { id: userId }
+          where: { id: userId },
         })
 
         if (!user) {
-            throw new NotFoundError('User not found')
+          throw new NotFoundError('User not found')
         }
 
         const invites = await prisma.invite.findMany({
-            where: {
-                email: user.email,
-            },
-            select: {
+          where: {
+            email: user.email,
+          },
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
                 id: true,
-                email: true,
-                role: true,
-                createdAt: true,
-                author: {
-                    select: {
-                        id: true,
-                        name: true,
-                    }
-                }
+                name: true,
+              },
             },
-            orderBy: {
-                createdAt: 'desc'
-            }
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
         })
 
-
         reply.status(200).send({ invites })
-    })
+      },
+    )
 }
